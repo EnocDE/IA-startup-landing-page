4ea3aa33dead3dc917601bed5a3136dea26fdea0
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-25 15:45:02 -0600

    Add scrolling animation to stars background

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import Button from "@/components/Button";
 import starsBg from "@/assets/stars.png";
 import gridLines from "@/assets/grid-lines.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
   return (
-    <section className="py-20 md:py-24">
+    <section ref={sectionRef} className="py-20 md:py-24">
       <div className="container">
-        <div
+        <motion.div
           className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
           style={{
             backgroundImage: `url(${starsBg.src})`,
+            backgroundPositionY,
+          }}
+          animate={{
+            backgroundPositionX: starsBg.width,
+          }}
+          transition={{
+            duration: 60,
+            repeat: Infinity,
+            ease: "linear",
           }}
         >
           <div
@@ -27,7 +50,7 @@ export const CallToAction = () => {
               <Button>Join waitlist</Button>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
